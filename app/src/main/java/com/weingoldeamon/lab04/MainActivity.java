3a7794faea53e00991701760c68b8aadbde4c3a7
eamonrw <eamonweingold@gmail.com>
2020-02-22 17:33:17 -0500

    Completed all activities Added swiping between them

--- app/src/main/java/com/weingoldeamon/lab04/MainActivity.java
+++ app/src/main/java/com/weingoldeamon/lab04/MainActivity.java
@@ -2,12 +2,30 @@ package com.weingoldeamon.lab04;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ViewFlipper;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewFlipper vflip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vflip = findViewById(R.id.view_flip);
+        getWindow().getDecorView().getRootView().setOnTouchListener(new OnSwipeTouchListener(MainActivity.this) {
+            public void onSwipeTop() {
+            }
+            public void onSwipeRight() {
+                vflip.showPrevious();
+            }
+            public void onSwipeLeft() {
+                vflip.showNext();
+            }
+            public void onSwipeBottom() {
+            }
+        });
     }
 }
